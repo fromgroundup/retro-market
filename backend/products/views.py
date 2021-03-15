ba5c040054147b518e9ac9f1057286680afb1c7e
fromgroundup <fromgroundup@protonmail.com>
2021-03-15 17:40:56 +1100

    Created rough model viewsets

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from rest_framework.permissions import AllowAny
+from .serializers import CategorySerializer, ProductSerializer, ListingSerializer
+from .models import Category, Product, Listing
 
-# Create your views here.
+class CategoryViewSet(ReadOnlyModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ListingViewSet(ModelViewSet):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
